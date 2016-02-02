892167fbd8a9c91c73f699769f5a62728b0aac9f
Ritesh Patel <ritzpatel91@gmail.com>
2016-02-03 00:13:28 +0530

    make separate static function to get ids of inactive sites

--- includes/class-mis-sites.php
+++ includes/class-mis-sites.php
@@ -19,57 +19,76 @@ if ( ! class_exists( 'MIS_Sites' ) ) {
 		 * Figure out inactive sub-sites and apply the action set into settings.
 		 */
 		public function manage_subsites() {
+
+			$mis_options = mis_get_options();
+			$site_action = $mis_options['action'];
+
+			// get ids of inactive sites
+			$inactive_site_ids = self::get_inactive_site_ids();
+
+			// check if we got any inactive sites
+			if ( ! empty( $inactive_site_ids ) && is_array( $inactive_site_ids ) ) {
+
+				foreach ( $inactive_site_ids as $site_id ) {
+
+					// do not let delete main site
+					if ( is_main_site( $site_id ) ) {
+						continue;
+					}
+
+					if ( 'archive' == $site_action ) {
+
+						// archive site
+						update_blog_status( $site_id, 'archived', '1' );
+
+					} elseif ( 'deactivate' == $site_action ) {
+
+						//deactivate site
+						do_action( 'deactivate_blog', $site_id );
+						update_blog_status( $site_id, 'deleted', '1' );
+
+					} elseif ( 'delete' == $site_action ) {
+
+						// delete site (remove tables as well)
+						if ( ! function_exists( 'wpmu_delete_blog' ) ) {
+							require_once( ABSPATH . '/wp-admin/includes/ms.php' );
+						}
+						wpmu_delete_blog( $site_id, true );
+					}
+
+					do_action( 'mis_inactive_subsite', $site_id, $site_action );
+				}
+			}
+		}
+
+
+		/**
+		 * Static function to get ids of inactive sites
+		 *
+		 * @return array
+		 */
+		public static function get_inactive_site_ids() {
+
 			global $wpdb;
 
 			$mis_options = mis_get_options();
 			$time_value = intval( $mis_options['time_value'] );
 			$time_unit = $mis_options['time_span'];
-			$site_action = $mis_options['action'];
+
+			$site_ids = array();
 
 			// check if time value and time unit is set or not
 			if ( $time_value > 0 && ! empty( $time_unit ) ) {
 
 				//todo prepare statement for $time_unit
-				$blog_ids = $wpdb->get_col(
+				$site_ids = $wpdb->get_col(
 					$wpdb->prepare(
 						"SELECT blog_id FROM $wpdb->blogs WHERE TIMESTAMPDIFF( {$time_unit}, registered, last_updated ) > %d", $time_value
 					)
 				);
-
-				// check if we got any inactive sites
-				if ( ! empty( $blog_ids ) && is_array( $blog_ids ) ) {
-
-					foreach ( $blog_ids as $blog_id ) {
-
-						// do not let delete main site
-						if ( is_main_site( $blog_id ) ) {
-							continue;
-						}
-
-						if ( 'archive' == $site_action ) {
-
-							// archive site
-							update_blog_status( $blog_id, 'archived', '1' );
-
-						} elseif ( 'deactivate' == $site_action ) {
-
-							//deactivate site
-							do_action( 'deactivate_blog', $blog_id );
-							update_blog_status( $blog_id, 'deleted', '1' );
-
-						} elseif ( 'delete' == $site_action ) {
-
-							// delete site (remove tables as well)
-							if ( ! function_exists( 'wpmu_delete_blog' ) ) {
-								require_once( ABSPATH . '/wp-admin/includes/ms.php' );
-							}
-							wpmu_delete_blog( $blog_id, true );
-						}
-
-						do_action( 'mis_inactive_subsite', $blog_id, $site_action );
-					}
-				}
 			}
+
+			return $site_ids;
 		}
 	}
 
